366e88473d0910a05e16bdc525405fa1995132ed
Yomna <yomnamussa@gmail.com>
2024-06-19 19:22:32 +0300

    Sara added user session and i editted in the login-signup route

--- routes/login-signup-route.js
+++ routes/login-signup-route.js
@@ -7,6 +7,9 @@ router.get('/', (req, res) => {
 
 const staticEmail = 'yomna@gmail.com';
 const staticPassword = 'password123';
+const staticUserEmail = 'sarah@gmail.com';
+const staticUserPassword = '1234';
+
 
 // User login route
 router.post('/login', (req, res) => {
@@ -17,23 +20,26 @@ router.post('/login', (req, res) => {
     req.session.user = { email: staticEmail };
     console.log('Session created:', req.session); // Log session info upon login
     res.redirect('/admin'); // Redirect to a protected page after login
-  } else {
-    res.status(401).send('Invalid email or password');
   }
-});
+    else if (email === staticUserEmail && password === staticUserPassword) {
+      req.session.user = { email: staticUserEmail, role: 'user' };
+      console.log('Session created:', req.session);
+      res.redirect('/user'); // Redirect to the user page after login
+    } 
+    else {
+      res.status(401).send('Invalid email or password');
+    }
+  });
 
 // User logout route
-
 router.get('/logout', (req, res) => {
-  console.log('Logout route hit'); // Debug log
   req.session.destroy((err) => {
     if (err) {
-      console.log('Error destroying session:', err); // Debug log
       return res.redirect('/admin');
     }
     res.clearCookie('connect.sid');
-    console.log('Session destroyed and cookie cleared'); // Debug log
     res.redirect('/login-signup');
   });
 });
+
 module.exports = router;
