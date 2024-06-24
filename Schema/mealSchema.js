9b6007ddb07701c5173ab0a5ff12b208061bc308
FadyNF <fadynf05@gmail.com>
2024-06-24 14:06:13 +0300

    phase 2

--- Schema/mealSchema.js
+++ Schema/mealSchema.js
@@ -4,7 +4,7 @@ const mealSchema = new mongoose.Schema(
   {
     name: {
       type: String,
-      required: [true, "Please enter a meal name"],
+      required: [false, "Please enter a meal name"],
     },
 
     image: {
@@ -14,112 +14,112 @@ const mealSchema = new mongoose.Schema(
 
     cookTime: {
       type: Number,
-      required: [true, "Please enter the duration (in minutes)"],
+      required: [false, "Please enter the duration (in minutes)"],
     },
 
     difficulty: {
       type: String,
-      required: [true, "Please enter the difficulty level"],
+      required: [false, "Please enter the difficulty level"],
       enum: ["Easy", "Medium", "Hard"],
     },
 
     description: {
       type: String,
-      required: [true, "Please enter a description"],
+      required: [false, "Please enter a description"],
     },
 
     tags: {
       type: String,
-      required: [true, "Please enter a cuisine"],
+      required: [false, "Please enter a cuisine"],
     },
     preferences: {
       type: [String],
-      required: [true, "Please enter from the 6 preferences"],
+      required: [false, "Please enter from the 6 preferences"],
       enum: ["meat", "veggies", "family", "fit", "speedy", "fish"],
       ref:"User"
     },
     ingredientsName: {
       type: [String],
-      required: [true, "Please enter ingredients name"],
+      required: [false, "Please enter ingredients name"],
     },
 
     ingPer2: {
       type: [String],
-      required: [true, "Please enter ingredients for 2 servings"],
+      required: [false, "Please enter ingredients for 2 servings"],
     },
 
     ingPer4: {
       type: [String],
-      required: [true, "Please enter ingredients for 4 servings"],
+      required: [false, "Please enter ingredients for 4 servings"],
     },
 
     instructions: {
       type: [String],
-      required: [true, "Please enter the instructions"],
+      required: [false, "Please enter the instructions"],
     },
     allergens: {
       type: [String],
-      required: [true, "Please enter allergens"],
+      required: [false, "Please enter allergens"],
     },
     utensils: {
       type: [String],
-      required: [true, "Please enter utensils"],
+      required: [false, "Please enter utensils"],
     },
     nutrition: {
       energy: {
         type: Number,
-        required: true,
+        required: false,
         min: [0, "Energy must be a positive number"],
       },
       calories: {
         type: Number,
-        required: true,
+        required: false,
         min: [0, "Calories must be a positive number"],
       },
       fat: {
         type: Number,
-        required: true,
+        required: false,
         min: [0, "Fats must be a positive number"],
       },
       satFat: {
         type: Number,
-        required: true,
+        required: false,
         min: [0, "satFat must be a positive number"],
       },
       carbohydrates: {
         type: Number,
-        required: true,
+        required: false,
         min: [0, "Carbohydrates must be a positive number"],
       },
       sugar: {
         type: Number,
-        required: true,
+        required: false,
         min: [0, "Sugar must be a positive number"],
       },
       fiber: {
         type: Number,
-        required: true,
+        required: false,
         min: [0, "Fiber must be a positive number"],
       },
       protein: {
         type: Number,
-        required: true,
+        required: false,
         min: [0, "Protein must be a positive number"],
       },
       cholesterol: {
         type: Number,
-        required: true,
+        required: false,
         min: [0, "Cholesterol must be a positive number"],
       },
       sodium: {
         type: Number,
-        required: true,
+        required: false,
         min: [0, "Sodium must be a positive number"],
       },
     },
     recommendations: {
       type: [String],
-      required: [true, "Please enter recommendations"],
+      required: [false, "Please enter recommendations"],
     },
   },
   { timestamps: true }
